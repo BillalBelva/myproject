064590f8f56a8286f014bc87a3c7b2a4bd9d69bb
bilalattaul <belvabillal@gmail.com>
2020-08-20 16:50:57 +0700

    add layout login

--- app/src/main/java/com/example/projectbaru/MainActivity.java
+++ app/src/main/java/com/example/projectbaru/MainActivity.java
@@ -1,47 +1,75 @@
 package com.example.projectbaru;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
 
 import android.graphics.Color;
-import android.os.Bundle;
+
 import android.view.View;
+
+import android.view.Menu;
+
+import android.view.MenuItem;
+
+import android.webkit.WebView;
+
+import android.webkit.WebViewClient;
+
 import android.widget.Button;
+
 import android.widget.EditText;
+
 import android.widget.TextView;
+
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    Button btnlogin;
+import java.io.FileInputStream;
 
-    EditText editUser, editPass;
+import java.io.FileOutputStream;
 
-    TextView textView;
+public class MainActivity extends Activity {
+
+
+
+    Button b1, b2;
+
+    EditText ed1, ed2;
+
+    TextView tx1;
 
 
 
     int counter = 3;
 
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        btnlogin = (Button) findViewById(R.id.btnLogin);
 
 
 
-        editUser = (EditText) findViewById(R.id.editUser);
+        b1 = (Button) findViewById(R.id.btnLogin);
 
-        editPass = (EditText) findViewById(R.id.editPass);
+        b2 = (Button) findViewById(R.id.btnCancel);
 
+        ed1 = (EditText) findViewById(R.id.editUser);
 
-
-        textView = (TextView) findViewById(R.id.textView2);
-
-        textView.setVisibility(View.GONE);
+        ed2 = (EditText) findViewById(R.id.editPass);
 
 
 
-        btnlogin.setOnClickListener(new View.OnClickListener() {
+        tx1 = (TextView) findViewById(R.id.textView2);
+
+        tx1.setVisibility(View.GONE);
+
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
 
             @Override
 
@@ -49,11 +77,11 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-                //set username dan password dengan "userbillal"
+                //set username dan password dengan "admin"
 
-                if (editUser.getText().toString().equals("userbillal") &&
+                if (ed1.getText().toString().equals("admin") &&
 
-                        editPass.getText().toString().equals("user1234"))
+                        ed2.getText().toString().equals("admin"))
 
 
 
@@ -73,19 +101,19 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-                    textView.setVisibility(View.VISIBLE);
+                    tx1.setVisibility(View.VISIBLE);
 
-                    textView.setBackgroundColor(Color.RED);
+                    tx1.setBackgroundColor(Color.RED);
 
                     counter--;
 
-                    textView.setText(Integer.toString(counter));
+                    tx1.setText(Integer.toString(counter));
 
 
 
                     if (counter == 0) {
 
-                        btnlogin.setEnabled(false);
+                        b1.setEnabled(false);
 
                     }
 
@@ -96,5 +124,21 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+
+
+        b2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                finish();
+
+            }
+
+        });
+
     }
+
 }
